0f770e0b3b2b9054d25ddf21c3e54033113f8d6d
glucrypto <glucrypto@protonmail.com>
2022-03-24 10:38:24 -0400

    added from lens, events and revert error

--- contracts/swaps/swap.sol
+++ contracts/swaps/swap.sol
@@ -1,19 +1,46 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 
 contract Swap is ERC721Enumerable {
 
+	// Original non-lens NFT
     address collectionNFTAddress;
+    // Lens Follow NFT
+    address followNFTAddress;
 
-    constructor(address _collectionNFTAddress) {
+    error NotNFTOwner();
+
+    event SwapToLens(address);
+    event SwapFromLens(address);
+
+    constructor(address _collectionNFTAddress, address _followNFTAddress) {
         collectionNFTAddress = _collectionNFTAddress;
+        followNFTAddress = _followNFTAddress;
+
     }
     
-    function swap(uint256 tokenId) external view {
-        require(IERC721(collectionNFTAddress).ownerOf(tokenId) == msg.sender, "Not an NFT owner");
-        IERC721(collectionNFTAddress).safeTransferFrom(msg.sender, collectionNFTAddress, tokenId);
+    /**
+	 * @notice Allows swap to lens NFT
+	 * @param tokenId of collection NFT you wish to swap
+	 * 
+	 */
+    function swapToLens(uint256 tokenId) external view {
+        if(IERC721(collectionNFTAddress).ownerOf(tokenId) == msg.sender) revert NotNFTOwner();
+        IERC721(collectionNFTAddress).safeTransferFrom(msg.sender, address(this), tokenId);
+        IERC721(followerNFTAddress).safeTransferFrom(address(this), msg.sender, tokenId);
+        emit SwapToLens(msg.sender);
+    }
 
+	/**
+	 * @notice Allows swap from lens
+	 * @param tokenId of follow NFT you wish to swap
+	 * 
+	 */
+    function swapFromLens(uint256 tokenId) external view {
+        if(IERC721(collectionNFTAddress).ownerOf(tokenId) == msg.sender) revert NotNFTOwner();
+        IERC721(followerNFTAddress).safeTransferFrom(msg.sender, address(this), tokenId);
+        IERC721(collectionNFTAddress).safeTransferFrom(address(this), msg.sender, tokenId);
+        emit SwapFromLens(msg.sender);
     }
 }
-
